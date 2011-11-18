9117f309d46df7b6e323cceee725321d8abcc6a3
CMGS <ilskdw@gmail.com>
2011-11-18 03:40:07 +0000

    fix bug

--- sheep/api/permdir.py
+++ sheep/api/permdir.py
@@ -11,7 +11,7 @@ permdir -- a string containing path to the directory holding permanent files.
 __all__ = ['permdir']
 
 import os
-from dae.util import find_app_root
+from sheep.util import find_app_root
 
 approot = os.environ.get('DAE_APPROOT') or find_app_root(raises=False) or ''
 permdir = os.path.join(approot, 'permdir')
