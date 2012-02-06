924b2a8dc130d5070ba4ad2c9fba37ed40b90c25
CMGS <ilskdw@gmail.com>
2012-02-06 17:31:22 +0000

    adjust sessions api and monkey

--- sheep/api/sessions.py
+++ sheep/api/sessions.py
@@ -2,3 +2,11 @@
 #coding:utf-8
 
 from werkzeug.contrib.sessions import *
+
+origin_session_middleware = SessionMiddleware.__init__
+def init(self, app, store, **kwargs):
+    kw = kwargs.copy()
+    kw.update(cookie_name='xid', \
+              environ_key='xiaomen.session')
+    return origin_session_middleware(self, app, store, **kw)
+SessionMiddleware.__init__ = init
