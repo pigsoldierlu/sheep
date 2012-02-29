123e110f2b849e582eef8e25b97e20b5133ed755
CMGS <ilskdw@gmail.com>
2012-02-29 11:57:43 +0000

    split files to static, upload and public

--- sheep/api/statics.py
+++ sheep/api/statics.py
@@ -5,12 +5,9 @@
 static_files url replace
 """
 
-import os
+__all__ = ['static_files', 'public_files', 'upload_files']
 
-__all__ = ['static_files']
-
-def static_files(path):
-    if os.environ.get('SHEEP_STATICS'):
-        appname = os.environ['SHEEP_APPNAME']
-        return os.path.join(os.environ['SHEEP_STATICS'], appname) + path
+def _files(path):
     return path
+
+static_files = public_files = upload_files = _files
