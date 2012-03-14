180960b69847467b2e721f9aafb23a96b96874b3
CMGS <ilskdw@gmail.com>
2012-03-14 08:37:28 +0000

    api.mysql default use pool

--- sheep/api/mysql.py
+++ sheep/api/mysql.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python
 # encoding: UTF-8
 
-from MySQLdb import *
+from pool.pool import QueuePool
+from MySQLdb import connect as _connect
+
+MAX_OVERFLOW = 20
+POOL_SIZE = 5
+TIMEOUT = 10
+
+pool = QueuePool(_connect, max_overflow=MAX_OVERFLOW, pool_size=POOL_SIZE, timeout=TIMEOUT)
+def connect():
+    return pool.connect()
+
