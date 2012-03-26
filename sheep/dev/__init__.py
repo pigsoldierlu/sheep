ae1da70862b79b2e12a52e491284f0c9d0dbff34
CMGS <ilskdw@gmail.com>
2012-03-26 03:57:20 +0000

    bug fix for manager not found

--- sheep/dev/__init__.py
+++ sheep/dev/__init__.py
@@ -4,11 +4,12 @@ import re
 
 def dispatcher(environ, start_response):
     path = environ['PATH_INFO']
-    pattern = re.compile('/_sheep/(?P<api>.*)/.*', re.I)
+    pattern = re.compile('/_sheep/(?P<api>.+?)/.*', re.I)
     match = pattern.match(path)
-    if not match or not match.groupdict():
-        return 'Not allowed'
-    api = match.groupdict()['api']
+    if not match:
+        start_response('404 Not Found', [])
+        return ['Not Found']
+    api = match.group('api')
     manager_module = __import__('', globals(), locals(), fromlist=[api],
                                 level=1)
     return getattr(manager_module, api).app(environ, start_response)
