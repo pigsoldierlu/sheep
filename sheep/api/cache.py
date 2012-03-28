3b5b6684aa1d801cd2d78128b96c8aa724faf81f
CMGS <ilskdw@gmail.com>
2012-03-28 09:02:02 +0000

    add delete method

--- sheep/api/cache.py
+++ sheep/api/cache.py
@@ -16,14 +16,7 @@ class Cache(object):
     def __call__(self, f):
         @functools.wraps(f)
         def wrapper(*args, **kwargs):
-            if args:
-                key = self._prefix + ':' + '-'.join(str(a) for a in args)
-            else:
-                key = self._prefix + ':'
-
-            if kwargs:
-                for k, v in kwargs.iteritems():
-                    key += '%s#%s' % (k, v)
+            key = self.generate_key(*args, **kwargs)
 
             value = self._cache.get(key)
             if not value:
@@ -38,4 +31,35 @@ class Cache(object):
                 logger.info('get cache %s' % key)
             return value
         return wrapper
+
+    def generate_key(self, *args, **kwargs):
+        if args:
+            key = self._prefix + ':' + '-'.join(str(a) for a in args)
+        else:
+            key = self._prefix + ':'
+
+        if kwargs:
+            for k, v in kwargs.iteritems():
+                key += '%s#%s' % (k, v)
+        return key
+
+    def delete(self, *args, **kwargs):
+        key = self.generate_key(*args, **kwargs)
+        self.delete_by_key(key)
+        logger.info('delete cache %s' % key)
+
+    def delete_multi(self, params=[]):
+        '''
+        params = [(args, kwargs), (args, kwargs)...]
+        '''
+        for param in params:
+            args, kwargs = param
+            self.delete(*args, **kwargs)
+
+    def delete_by_key(self, key):
+        self._cache.delete(key)
+
+    def delete_multi_by_key(self, keys):
+        for key in keys:
+            self.delete_by_key(key)
 
