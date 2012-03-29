aadb29c52c3776af27304d41e49fa5eded60e7cd
CMGS <ilskdw@gmail.com>
2012-03-29 11:05:31 +0000

    change cache

--- sheep/api/cache.py
+++ sheep/api/cache.py
@@ -1,65 +1,116 @@
 #!/usr/local/bin/python2.7
 #coding:utf-8
 
+import re
+import inspect
 import logging
-import functools
-from werkzeug.contrib import cache
+from functools import wraps
+from werkzeug.contrib import cache as _cache
 
 logger = logging.getLogger(__name__)
 
-class Cache(object):
-    def __init__(self, prefix, timeout=0):
-        self._cache = cache.FileSystemCache('/tmp')
-        self._prefix = prefix
-        self._timeout = timeout
+old_pattern = re.compile(r'%\w')
+new_pattern = re.compile(r'\{(\w+(\.\w+|\[\w+\])?)\}')
 
-    def __call__(self, f):
-        @functools.wraps(f)
-        def wrapper(*args, **kwargs):
-            key = self.generate_key(*args, **kwargs)
+__formaters = {}
 
-            value = self._cache.get(key)
-            if not value:
-                value = f(*args, **kwargs)
-                try:
-                    logger.info('set cache %s' % key)
-                    self._cache.set(key, value, self._timeout)
-                except Exception, e:
-                    logger.warn('cache error: %s, exception: %s' % (key, e))
-                    pass
-            else:
-                logger.info('get cache %s' % key)
-            return value
-        return wrapper
+backend = _cache.FileSystemCache('/tmp')
 
-    def generate_key(self, *args, **kwargs):
-        if args:
-            key = self._prefix + ':' + '-'.join(str(a) for a in args)
+def gen_key(key_pattern, arg_names, defaults, *a, **kw):
+    return gen_key_factory(key_pattern, arg_names, defaults)(*a, **kw)
+
+def gen_key_factory(key_pattern, arg_names, defaults):
+    args = dict(zip(arg_names[-len(defaults):], defaults)) if defaults else {}
+    if callable(key_pattern):
+        names = inspect.getargspec(key_pattern)[0]
+    def gen_key(*a, **kw):
+        aa = args.copy()
+        aa.update(zip(arg_names, a))
+        aa.update(kw)
+        if callable(key_pattern):
+            key = key_pattern(*[aa[n] for n in names])
         else:
-            key = self._prefix + ':'
+            key = format(key_pattern, *[aa[n] for n in arg_names], **aa)
+        return key and key.replace(' ','_'), aa
+    return gen_key
 
-        if kwargs:
-            for k, v in kwargs.iteritems():
-                key += '%s#%s' % (k, v)
-        return key
+def cache(key_pattern, expire=0, max_retry=0):
+    def deco(f):
+        arg_names, varargs, varkw, defaults = inspect.getargspec(f)
+        if varargs or varkw:
+            raise Exception("do not support varargs")
+        gen_key = gen_key_factory(key_pattern, arg_names, defaults)
+        @wraps(f)
+        def _(*a, **kw):
+            key, args = gen_key(*a, **kw)
+            if not key:
+                return f(*a, **kw)
+            logger.info('get key %s', key)
+            r = backend.get(key)
 
-    def delete(self, *args, **kwargs):
-        key = self.generate_key(*args, **kwargs)
-        self.delete_by_key(key)
-        logger.info('delete cache %s' % key)
+            # anti miss-storm
+            retry = max_retry
+            while r is None and retry > 0:
+                # when node is down, add() will failed
+                if backend.add(key + '#mutex', 1, int(max_retry * 0.1)):
+                    break
+                time.sleep(0.1)
+                r = backend.get(key)
+                retry -= 1
 
-    def delete_multi(self, params=[]):
-        '''
-        params = [(args, kwargs), (args, kwargs)...]
-        '''
-        for param in params:
-            args, kwargs = param
-            self.delete(*args, **kwargs)
+            if r is None:
+                r = f(*a, **kw)
+                if r is not None:
+                    logger.info('set key %s', key)
+                    backend.set(key, r, expire)
+                if max_retry > 0:
+                    backend.delete(key + '#mutex')
+            return r
+        _.original_function = f
+        return _
+    return deco
 
-    def delete_by_key(self, key):
-        self._cache.delete(key)
+def format(text, *a, **kw):
+    f = __formaters.get(text)
+    if f is None:
+        f = formater(text)
+        __formaters[text] = f
+    return f(*a, **kw)
 
-    def delete_multi_by_key(self, keys):
-        for key in keys:
-            self.delete_by_key(key)
+def formater(text):
+    """
+    >>> format('%s %s', 3, 2, 7, a=7, id=8)
+    '3 2'
+    >>> format('%(a)d %(id)s', 3, 2, 7, a=7, id=8)
+    '7 8'
+    >>> format('{1} {id}', 3, 2, a=7, id=8)
+    '2 8'
+    >>> class Obj: id = 3
+    >>> format('{obj.id} {0.id}', Obj(), obj=Obj())
+    '3 3'
+    """
+    def translator(k):
+        if '.' in k:
+            name,attr = k.split('.')
+            if name.isdigit():
+                k = int(name)
+                return lambda *a, **kw: getattr(a[k], attr)
+            return lambda *a, **kw: getattr(kw[name], attr)
+        else:
+            if k.isdigit():
+                return lambda *a, **kw: a[int(k)]
+            return lambda *a, **kw: kw[k]
+    args = [translator(k) for k,_1 in new_pattern.findall(text)]
+    if args:
+        if old_pattern.findall(text):
+            raise Exception('mixed format is not allowed')
+        f = new_pattern.sub('%s', text)
+        def _(*a, **kw):
+            return f % tuple([k(*a,**kw) for k in args])
+        return _
+    elif '%(' in text:
+        return lambda *a, **kw: text % kw 
+    else:
+        n = len(old_pattern.findall(text))
+        return lambda *a, **kw: text % tuple(a[:n])
 
