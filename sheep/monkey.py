7d9d4f00adb3efeb941cffba53bc06f22b04b700
CMGS <ilskdw@gmail.com>
2012-04-16 10:41:24 +0000

    refactor monkey patch

--- sheep/monkey.py
+++ sheep/monkey.py
@@ -5,11 +5,34 @@
 
 from .util import load_dev_config, load_app_config
 
-def patch_MySQLdb(approot):
-    from pymysql import install_as_MySQLdb
-    install_as_MySQLdb()
+def use_pymysql():
+    try:
+        import sys
+        from pymysql import converters
+        from pymysql import install_as_MySQLdb
+        from pymysql.connections import Connection
+        from pymysql.converters import escape_string
 
+        if not getattr(converters, 'Thing2Literal', None):
+            def Thing2Literal(o, d):
+                return "'%s'" % escape_string(str(o))
+            setattr(converters, 'Thing2Literal', Thing2Literal)
+            sys.modules['MySQLdb.converters'] = sys.modules['pymysql.converters']
+
+        if not getattr(Connection, 'select_db', None):
+            def select_db(self, db):
+                self.db = db
+                self.query("USE %s" % db)
+            setattr(Connection, 'select_db', select_db)
+    except ImportError:
+        pass
+    else:
+        install_as_MySQLdb()
+
+def patch_MySQLdb(approot):
+    use_pymysql()
     devcfg = load_dev_config(approot)
+
     import MySQLdb
     if getattr(MySQLdb, 'sheep_patched', False):
         return
