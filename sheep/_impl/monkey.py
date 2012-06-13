53681c3f5c03a45246177c55c9f197d57515b0df
CMGS <ilskdw@mspil.edu.cn>
2012-06-13 14:03:28 +0800

    add _impl

---
+++ sheep/_impl/monkey.py
@@ -0,0 +1,9 @@
+import os
+
+def get_app_mysql_config():
+    from sheep.util import load_dev_config
+    devcfg = load_dev_config(os.environ['SHEEP_APPROOT']) or {}
+    return devcfg.get('mysql', {})
+
+def patch_impl():
+    pass
