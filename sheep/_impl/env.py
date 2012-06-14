abef07352cf73be24065aef29442110546222a01
CMGS <ilskdw@mspil.edu.cn>
2012-06-14 11:44:33 +0800

    bug fix

--- sheep/_impl/env.py
+++ sheep/_impl/env.py
@@ -4,10 +4,17 @@ import os
 import pkg_resources
 
 def init():
-    dist = pkg_resources.get_distribution('sheep')
     os.environ['SHEEP_ENV'] = 'SDK'
-    os.environ['SHEEP_ENV_VERSION'] = dist.version
-    os.environ['SHEEP_SDK_PATH'] = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+    try:
+        dist = pkg_resources.get_distribution('sheep')
+        os.environ['SHEEP_ENV_VERSION'] = dist.version
+    except Exception:
+        os.environ['SHEEP_ENV_VERSION'] = 'unknown'
+
+    os.environ['SHEEP_SDK_PATH'] = os.path.abspath(
+            os.path.dirname(                        # sheep/
+                os.path.dirname(                    # sheep/sheep/
+                    os.path.dirname(__file__))))    # sheep/sheep/_impl/
 
 
 def activate_app():
