2fc0f4ad84926f2eb8fb9f3463c58ce99b0bed55
CMGS <ilskdw@mspil.edu.cn>
2012-06-27 14:34:31 +0800

    add plugin list

--- sheep/commands/plugin.py
+++ sheep/commands/plugin.py
@@ -3,6 +3,7 @@
 
 import os
 import logging
+import pkg_resources
 from subprocess import call
 
 logger = logging.getLogger(__name__)
@@ -12,8 +13,13 @@ def populate_argument_parser(parser):
     parser.add_argument('action')
 
 def main(args):
-    if args.action not in ['install', 'develop']:
+    if args.action not in ['list', 'install', 'develop']:
         logger.error('action error')
+        return 0
+
+    if args.action == 'list':
+        for ep in pkg_resources.iter_entry_points('dae.plugins'):
+            logger.info(ep.name)
         return 0
 
     python_path = os.path.join(os.environ['SHEEP_SDK_PATH'], 'venv', 'bin', 'python')
